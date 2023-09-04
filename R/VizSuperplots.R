94c6e4775540def06e2dd5253a8e70dc9e944abb
Dimitrios Prymidis <dprymidis@gmail.com>
2023-09-04 14:16:26 +0200

    Superplots Fix

--- R/VizSuperplots.R
+++ R/VizSuperplots.R
@@ -25,9 +25,9 @@
 ###################################
 
 #' @param Input_data DF with unique sample identifiers as row names and metabolite numerical values in columns with metabolite identifiers as column names. Includes experimental design and outlier column.
-#' @param Experimental_design DF which contains information about the samples, which will be combined with your input data based on the unique sample identifiers used as rownames. Column "Conditions" with information about the sample conditions (e.g. "N" and "T" or "Normal" and "Tumor"), can be used for feature filtering and colour coding in the PCA. Column "AnalyticalReplicate" including numerical values, defines technical repetitions of measurements, which will be summarised. Column "BiologicalReplicates" including numerical values. Please use the following names: "Conditions", "Biological_Replicates", "Analytical_Replicates".
+#' @param Input_SettingsFile DF which contains information about the samples, which will be combined with your input data based on the unique sample identifiers used as rownames. Column "Conditions" with information about the sample conditions (e.g. "N" and "T" or "Normal" and "Tumor"), can be used for feature filtering and colour coding in the PCA. Column "AnalyticalReplicate" including numerical values, defines technical repetitions of measurements, which will be summarised. Column "BiologicalReplicates" including numerical values. Please use the following names: "Conditions", "Biological_Replicates", "Analytical_Replicates".
 #' @param Graprh_Style String with the information of the Graph style. Available options are Bar. Box and Violin  \strong{Default = Box}
-#' @param Superplot \emph{Optional: } String with a Column name of the Experimental_design as string which is used to make the plots Superplots.
+#' @param Superplot \emph{Optional: } String with a Column name of the Input_SettingsFile as string which is used to make the plots Superplots.
 #' @param Output_Name \emph{Optional: } String which is added to the output files of the plot.
 #' @param Output_plots String with plot save information. Available options are "Individual" for plots of each Individual metabolite and "Together" for a pdf containing all the plots. \strong{Default = Together}
 #' @param Selected_Conditions Vector with names of selected Conditions for the plot. \strong{Default = NULL}
@@ -39,7 +39,7 @@
 #' @export
 
 VizSuperplot <- function(Input_data,
-                     Experimental_design,
+                     Input_SettingsFile,
                      Graprh_Style = "Box", # Bar, Box, Violin
                      Superplot = NULL,
                      OutputPlotName = "",
@@ -60,21 +60,21 @@ VizSuperplot <- function(Input_data,
   #1. Input_data and Conditions
   if(any(duplicated(row.names(Input_data)))==TRUE){
     stop("Duplicated row.names of Input_data, whilst row.names must be unique")
-  } else if("Conditions" %in% colnames(Experimental_design)==FALSE){
-    stop("There is no column named `Conditions` in Experimental_design to obtain Condition1 and Condition2.")
+  } else if("Conditions" %in% colnames(Input_SettingsFile)==FALSE){
+    stop("There is no column named `Conditions` in Input_SettingsFile to obtain Condition1 and Condition2.")
   } else{
     Test_num <- apply(Input_data, 2, function(x) is.numeric(x))
     if((any(Test_num) ==  FALSE) ==  TRUE){
       stop("Input_data needs to be of class numeric")
     } else{
-      Test_match <- merge(Experimental_design, Input_data, by.x = "row.names",by.y = "row.names", all =  FALSE) # Do the unique IDs of the "Input_data" match the row names of the "Experimental_design"?
+      Test_match <- merge(Input_SettingsFile, Input_data, by.x = "row.names",by.y = "row.names", all =  FALSE) # Do the unique IDs of the "Input_data" match the row names of the "Input_SettingsFile"?
       if(nrow(Test_match) ==  0){
-        stop("row.names Input_data need to match row.names Experimental_design.")
+        stop("row.names Input_data need to match row.names Input_SettingsFile.")
       } else(
         data <- Input_data
       )
     }
-    Experimental_design <- Experimental_design
+    Input_SettingsFile <- Input_SettingsFile
   }
 
   #2. Plot options
@@ -82,8 +82,8 @@ VizSuperplot <- function(Input_data,
     stop("Graprh_Style must be either Box, Bar ot Violin.")
   }
   if(is.null(Superplot)==FALSE){
-    if(Superplot %in% colnames(Experimental_design) == FALSE){
-      stop("Superplot is active. However, the column ", paste(Superplot), " is not the in Experimental_design." )
+    if(Superplot %in% colnames(Input_SettingsFile) == FALSE){
+      stop("Superplot is active. However, the column ", paste(Superplot), " is not the in Input_SettingsFile." )
     }
   }
 
@@ -91,14 +91,14 @@ VizSuperplot <- function(Input_data,
   if (Output_plots %in% Output_plots_options == FALSE){
     stop("Check Input the Plot_pathways option is incorrect. The Allowed options are the following: ",paste(Output_plots_options,collapse = ", "),"." )
   }
-  if("Conditions" %in% colnames(Experimental_design)==FALSE){
+  if("Conditions" %in% colnames(Input_SettingsFile)==FALSE){
     stop("There is no column named `Conditions` in Input_data.")
   }
 
   #3. Comparison options
   if(is.null(Selected_Conditions)==FALSE){
     for (Conditions in Selected_Conditions){
-      if(Conditions %in% Experimental_design$Conditions==FALSE){
+      if(Conditions %in% Input_SettingsFile$Conditions==FALSE){
         stop("Check Input. The Selected_Conditions were not found in the Conditions Column.")
       }
     }
@@ -106,11 +106,11 @@ VizSuperplot <- function(Input_data,
 
   if(is.null(Selected_Comparisons)==FALSE){
     for (Comp in Selected_Comparisons){
-      if(Comp[1] %in% Experimental_design$Conditions ==FALSE){
-        stop("Check Input. The Selected_Comparisons condition ",paste(Comp[1]), " is not found in the Conditions Column of the Experimental_design.")
+      if(Comp[1] %in% Input_SettingsFile$Conditions ==FALSE){
+        stop("Check Input. The Selected_Comparisons condition ",paste(Comp[1]), " is not found in the Conditions Column of the Input_SettingsFile.")
       }
-      if(Comp[2] %in% Experimental_design$Conditions ==FALSE){
-        stop("Check Input. The Selected_Comparisons condition ",paste(Comp[2]), " is not found in the Conditions Column of the Experimental_design.")
+      if(Comp[2] %in% Input_SettingsFile$Conditions ==FALSE){
+        stop("Check Input. The Selected_Comparisons condition ",paste(Comp[2]), " is not found in the Conditions Column of the Input_SettingsFile.")
       }
     }
   }
@@ -135,15 +135,21 @@ VizSuperplot <- function(Input_data,
     if (!dir.exists(Results_folder)) {dir.create(Results_folder)} # Make Results folder
     Results_folder_plots_Barplots_folder = file.path(Results_folder, paste(Graprh_Style, "plots", sep=""))
     if (!dir.exists(Results_folder_plots_Barplots_folder)) {dir.create(Results_folder_plots_Barplots_folder)}  # check and create folder
+
+    if(Output_plots == "Individual"){
+      Results_folder_plots_Barplots_folder_Individual = file.path(Results_folder_plots_Barplots_folder, "Individual")
+      if (!dir.exists(Results_folder_plots_Barplots_folder_Individual)) {dir.create(Results_folder_plots_Barplots_folder_Individual)}  # check and create folder
+
+    }
   }
 
 
   Metabolite_Names <- colnames(data)
-  data <- merge( Experimental_design[c("Conditions",Superplot)] ,data,, by=0)
+  data <- merge( Input_SettingsFile[c("Conditions",Superplot)] ,data, by=0)
   data <- column_to_rownames(data, "Row.names")
 
   # make a list for plotting all plots together
-  outlier_plot_list <- list()
+  plot_list <- list()
   k=1
 
   for (i in Metabolite_Names){
@@ -166,16 +172,11 @@ VizSuperplot <- function(Input_data,
 
     # Add graph style
     if (Graprh_Style == "Bar"){
-      Plot <- Plot+
-        geom_bar(stat = "summary", fun = "mean", fill = "skyblue") +
-        geom_errorbar(data = dataMeans, aes(x=Conditions, ymin=Intensity-sd, ymax=Intensity+sd), width=0.4, colour="black", alpha=0.9, size=0.5)
+      Plot <- Plot+  geom_bar(stat = "summary", fun = "mean", fill = "skyblue")
     } else if (Graprh_Style == "Violin"){
-      Plot <- Plot+
-        geom_violin(fill = "skyblue")  +
-        geom_errorbar(data = dataMeans, aes(x=Conditions, ymin=Intensity-sd, ymax=Intensity+sd), width=0.4, colour="black", alpha=0.9, size=0.5)
+      Plot <- Plot+ geom_violin(fill = "skyblue")
     } else if (Graprh_Style == "Box"){
-      Plot <- Plot +
-        geom_boxplot(fill="skyblue")
+      Plot <- Plot +  geom_boxplot(fill="skyblue")
     }
 
     # Add superplot
@@ -208,9 +209,9 @@ VizSuperplot <- function(Input_data,
 
       if (!is.null(Save_as_Plot)) {
         if(OutputPlotName ==""){
-          ggsave(file=paste(Results_folder_plots_Barplots_folder, "/",i, ".",Save_as_Plot, sep=""), plot=Plot, width=10, height=8)
+          ggsave(file=paste(Results_folder_plots_Barplots_folder_Individual, "/",i, ".",Save_as_Plot, sep=""), plot=Plot, width=10, height=8)
         }else{
-          ggsave(file=paste(Results_folder_plots_Barplots_folder, "/",OutputPlotName,"_",i, ".",Save_as_Plot, sep=""), plot=Plot, width=10, height=8)
+          ggsave(file=paste(Results_folder_plots_Barplots_folder_Individual, "/",OutputPlotName,"_",i, ".",Save_as_Plot, sep=""), plot=Plot, width=10, height=8)
         }
       }
 
@@ -218,7 +219,7 @@ VizSuperplot <- function(Input_data,
     } else if(Output_plots=="Together"){
 
       plot(Plot)
-      outlier_plot_list[[k]] <- recordPlot()
+      plot_list[[k]] <- recordPlot()
       dev.off()
       k=k+1
     }
@@ -230,7 +231,7 @@ VizSuperplot <- function(Input_data,
         pdf(file= paste(Results_folder_plots_Barplots_folder,"/",Graprh_Style, "plots", OutputPlotName,".pdf", sep = ""), onefile = TRUE )
       }else{
         pdf(file= paste(Results_folder_plots_Barplots_folder,"/",Graprh_Style, "plots", OutputPlotName,".pdf", sep = ""), onefile = TRUE )
-        for (plot in outlier_plot_list){
+        for (plot in plot_list){
           replayPlot(plot)
         }
         dev.off()
