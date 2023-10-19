b9d067d43e6a661cabc72d3bf41aef6028eb2474
Christina Schmidt <christina.schmidt1@outlook.de>
2023-10-19 11:04:43 +0200

    Chaching results of KEGG #36

--- R/Helper.R
+++ R/Helper.R
@@ -61,7 +61,7 @@ toy_data <- function(data) {
 #' @return A data frame containing the toy data.
 #' @export
 #'
-MCA_rules<- function(data) {
+MCA_rules<- function(data){
   # Read the .csv files
   Cond <- system.file("data", "MCA_2Cond.csv", package = "MetaProViz")
   Cond<- read.csv( Cond, check.names=FALSE)
@@ -91,68 +91,85 @@ MCA_rules<- function(data) {
 #'
 Load_KEGG<- function(){
   #Get the package:
-  RequiredPackages <- c("KEGGREST", "tidyverse")
+  RequiredPackages <- c("rappdirs")
   new.packages <- RequiredPackages[!(RequiredPackages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) install.packages(new.packages)
 
-  suppressMessages(library(KEGGREST))
-  suppressMessages(library(tidyverse))
+  #------------------------------------------------------------------
+  #Get the directory and filepath of cache results of R
+  directory <- rappdirs::user_cache_dir()#get chache directory
+  File_path <-paste(directory, "/KEGG_Metabolite.rds", sep="")
 
-  # 1. Make a list of all available human pathways in KEGG
-  Pathways_H <- as.data.frame(keggList("pathway", "hsa"))  # hsa = human
+  if(file.exists(File_path)==TRUE){# First we will check the users chache directory and weather there are rds files with KEGG_pathways already:
+    KEGG_Metabolite <- readRDS(File_path)
+    message("Cached file loaded from: ", File_path)
+  }else{# load from KEGG
+    RequiredPackages <- c("KEGGREST", "tidyverse")
+    new.packages <- RequiredPackages[!(RequiredPackages %in% installed.packages()[,"Package"])]
+    if(length(new.packages)) install.packages(new.packages)
 
-  # 2. Initialize the result data frame
-  KEGG_H <- data.frame(KEGGPathway = character(nrow(Pathways_H)),
-                       #PathID = 1:nrow(Pathways_H),
-                       Compound = 1:nrow(Pathways_H),
-                       KEGG_CompoundID = 1:nrow(Pathways_H),
-                       stringsAsFactors = FALSE)
+    suppressMessages(library(KEGGREST))
+    suppressMessages(library(tidyverse))
 
-  # 3. Iterate over each pathway and extract the needed information
-  for (k in 1:nrow(Pathways_H)) {
-    path <- rownames(Pathways_H)[k]
+    #--------------------------------------------------------------------------------------------
+    # 1. Make a list of all available human pathways in KEGG
+    Pathways_H <- as.data.frame(keggList("pathway", "hsa"))  # hsa = human
 
-    tryCatch({#try-catch block is used to catch any errors that occur during the query process
-      # Query the pathway information
-      query <- keggGet(path)
+    # 2. Initialize the result data frame
+    KEGG_H <- data.frame(KEGGPathway = character(nrow(Pathways_H)),
+                         #PathID = 1:nrow(Pathways_H),
+                         Compound = 1:nrow(Pathways_H),
+                         KEGG_CompoundID = 1:nrow(Pathways_H),
+                         stringsAsFactors = FALSE)
 
-      # Extract the necessary information and store it in the result data frame
-      KEGG_H[k, "KEGGPathway"] <- Pathways_H[k,]
-      #KEGG_H[k, "PathID"] <- path
-      KEGG_H[k, "Compound"] <- paste(query[[1]]$COMPOUND, collapse = ";")
-      KEGG_H[k, "KEGG_CompoundID"] <- paste(names(query[[1]]$COMPOUND), collapse = ";")
-    }, error = function(e) {
-      # If an error occurs, store "error" in the corresponding row and continue to the next query
-      KEGG_H[k, "KEGGPathway"] <- "error"
-      message(paste("`Error in .getUrl(url, .flatFileParser) : Not Found (HTTP 404).` for pathway", path, "- Skipping and continuing to the next query."))
-    })
+    # 3. Iterate over each pathway and extract the needed information
+    for (k in 1:nrow(Pathways_H)) {
+      path <- rownames(Pathways_H)[k]
+
+      tryCatch({#try-catch block is used to catch any errors that occur during the query process
+        # Query the pathway information
+        query <- keggGet(path)
+
+        # Extract the necessary information and store it in the result data frame
+        KEGG_H[k, "KEGGPathway"] <- Pathways_H[k,]
+        #KEGG_H[k, "PathID"] <- path
+        KEGG_H[k, "Compound"] <- paste(query[[1]]$COMPOUND, collapse = ";")
+        KEGG_H[k, "KEGG_CompoundID"] <- paste(names(query[[1]]$COMPOUND), collapse = ";")
+      }, error = function(e) {
+        # If an error occurs, store "error" in the corresponding row and continue to the next query
+        KEGG_H[k, "KEGGPathway"] <- "error"
+        message(paste("`Error in .getUrl(url, .flatFileParser) : Not Found (HTTP 404).` for pathway", path, "- Skipping and continuing to the next query."))
+      })
+    }
+
+    # 3. Remove the pathways that do not have any metabolic compounds associated to them
+    KEGG_H_Select <-KEGG_H%>%
+      subset(!KEGG_CompoundID=="")%>%
+      subset(!KEGGPathway=="")
+
+    # 4. Make the Metabolite DF
+    KEGG_Metabolite <- separate_longer_delim(KEGG_H_Select[,-5], c(Compound, KEGG_CompoundID), delim = ";")
+
+    # 5. Remove Metabolites
+    ### 5.1. Ions should be removed
+    Remove_Ions <- c("Calcium cation","Potassium cation","Sodium cation","H+","Cl-", "Fatty acid", "Superoxide","H2O", "CO2", "Copper", "Fe2+", "Magnesium cation", "Fe3+",  "Zinc cation", "Nickel", "NH4+")
+    ### 5.2. Unspecific small molecules
+    Remove_Small <- c("Nitric oxide","Hydrogen peroxide", "Superoxide","H2O", "CO2", "Hydroxyl radical", "Ammonia", "HCO3-",  "Oxygen", "Diphosphate", "Reactive oxygen species", "Nitrite", "Nitrate", "Hydrogen", "RX", "Hg")
+
+    KEGG_Metabolite <- KEGG_Metabolite[!(KEGG_Metabolite$Compound %in% c(Remove_Ions, Remove_Small)), ]
+
+    #Change syntax as required for ORA
+    KEGG_Metabolite <- KEGG_Metabolite%>%
+      dplyr::rename("term"=1,
+                    "Metabolite"=2,
+                    "MetaboliteID"=3)
+    KEGG_Metabolite$Description <- KEGG_Metabolite$term
+
+    #Save the results as an RDS file in the Cache directory of R
+    if(!dir.exists(directory)) {dir.create(directory)}
+    saveRDS(KEGG_Metabolite, file = paste(directory, "/KEGG_Metabolite.rds", sep=""))
   }
-
-  # 3. Remove the pathways that do not have any metabolic compounds associated to them
-  KEGG_H_Select <-KEGG_H%>%
-    subset(!KEGG_CompoundID=="")%>%
-    subset(!KEGGPathway=="")
-
-  # 4. Make the Metabolite DF
-  KEGG_Metabolite <- separate_longer_delim(KEGG_H_Select[,-5], c(Compound, KEGG_CompoundID), delim = ";")
-
-  # 5. Remove Metabolites
-  ### 5.1. Ions should be removed
-  Remove_Ions <- c("Calcium cation","Potassium cation","Sodium cation","H+","Cl-", "Fatty acid", "Superoxide","H2O", "CO2", "Copper", "Fe2+", "Magnesium cation", "Fe3+",  "Zinc cation", "Nickel", "NH4+")
-  ### 5.2. Unspecific small molecules
-  Remove_Small <- c("Nitric oxide","Hydrogen peroxide", "Superoxide","H2O", "CO2", "Hydroxyl radical", "Ammonia", "HCO3-",  "Oxygen", "Diphosphate", "Reactive oxygen species", "Nitrite", "Nitrate", "Hydrogen", "RX", "Hg")
-
-  KEGG_Metabolite <- KEGG_Metabolite[!(KEGG_Metabolite$Compound %in% c(Remove_Ions, Remove_Small)), ]
-
-  #Change syntax as required for ORA
-  KEGG_Metabolite <- KEGG_Metabolite%>%
-    dplyr::rename("term"=1,
-                  "Metabolite"=2,
-                  "MetaboliteID"=3)
-  KEGG_Metabolite$Description <- KEGG_Metabolite$term
-
 
   #Return into environment
   assign("KEGG_Pathways", KEGG_Metabolite, envir=.GlobalEnv)
-
 }
