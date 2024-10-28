bc814bfc3c7533ad8f1f74bfc45877b22ff9492b
Christina Schmidt <christina.schmidt1@outlook.de>
2024-10-28 11:34:40 +0100

    InputCheck rownames

--- tests/testthat/test-CheckInputs.R
+++ tests/testthat/test-CheckInputs.R
@@ -1,4 +1,8 @@
-test_that("multiplication works", {
+test_that("InputChecks", {
   d <- ToyData('IntraCells_Raw')
   expect_error(CheckInput(d), 'needs to be of class numeric')
+
+  d_meta <-d[,c(1:3)]%>%
+    tibble::remove_rownames()
+  expect_error(CheckInput(d, InputData_Num=FALSE, SettingsFile_Sample=d_meta), 'row.names InputData need to match row.names SettingsFile_Sample.')
 })
