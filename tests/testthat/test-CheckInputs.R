1342cbcab21db2d538f6fe3d43a3c3b2f220de6f
Christina Schmidt <christina.schmidt1@outlook.de>
2024-10-31 13:20:36 +0100

    Added :: and ::: syntax

--- tests/testthat/test-CheckInputs.R
+++ tests/testthat/test-CheckInputs.R
@@ -1,8 +1,8 @@
 test_that("InputChecks", {
-  d <- ToyData('IntraCells_Raw')
-  expect_error(CheckInput(d), 'needs to be of class numeric')
+  d <- MetaProViz::ToyData('IntraCells_Raw')
+  expect_error(MetaProViz:::CheckInput(d), 'needs to be of class numeric')
 
   d_meta <-d[,c(1:3)]%>%
     tibble::remove_rownames()
-  expect_error(CheckInput(d, InputData_Num=FALSE, SettingsFile_Sample=d_meta), 'row.names InputData need to match row.names SettingsFile_Sample.')
+  expect_error(MetaProViz:::CheckInput(d, InputData_Num=FALSE, SettingsFile_Sample=d_meta), 'row.names InputData need to match row.names SettingsFile_Sample.')
 })
