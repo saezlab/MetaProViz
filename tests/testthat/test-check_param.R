945d9ff163ed3920089cf23f73e7e899155e4f53
Christina Schmidt <christina.schmidt1@outlook.de>
2025-07-31 14:07:14 +0200

    Creation of new, empty branch

--- tests/testthat/test-check_param.R
+++
@@ -1,8 +0,0 @@
-test_that("check_param", {
-  d <- intracell_raw %>% tibble::column_to_rownames("Code")
-  expect_error(MetaProViz:::check_param(d), 'needs to be of class numeric')
-
-  d_meta <-d[,c(1:3)] %>%
-    tibble::remove_rownames()
-  expect_error(MetaProViz:::check_param(d, data_num=FALSE, metadata_sample=d_meta), 'row.names data need to match row.names metadata_sample.')
-})
