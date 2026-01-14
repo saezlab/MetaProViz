6e24e279efd1b89306314d7511efccee1a8ceb30
Jannik Franken <jannik8513@gmailcom>
2026-01-14 16:15:59 +0000

    more testing

--- cluster_pk/test_cluster_pk_plots.R
+++ cluster_pk/test_cluster_pk_plots.R
@@ -24,11 +24,14 @@ r <- cluster_pk(
     ),
     input_format = "long",
     similarity = "jaccard",
-    threshold = 0.5,
-    clust = "components",
+    threshold = 0.2,
+    clust = "community",
     min = 2,
+    plot_name = "Long",
     save_plot = "png",
     min_degree = 1,
+    seed = 123,
+    show_density = TRUE,
     max_nodes = 1000
 ) 
 
@@ -37,14 +40,16 @@ print(r$graph_plot)
 
 
 
-
+source("R/VizGraph.R")
+source("cluster_pk/cluster_pk.R")
 
 
 ## test for enrichment results
 
 d_enrichment <- read.csv("D:/Masterarbeit_LOCAL/enrichment_results_test.csv", sep=";")
-
 names(d_enrichment)[1] <- "ID"
+
+# d_enrichment[1, "percentage_of_Pathway_detected"] <- 100
 
 r_enrichment <- cluster_pk(
     d_enrichment,
@@ -55,34 +60,31 @@ r_enrichment <- cluster_pk(
     input_format = "enrichment",
     similarity = "jaccard",
     threshold = 0.5,
-    clust = "components",
+    clust = "community",
     min = 2,
-    save_plot = NULL,
-    print_plot = TRUE,
+    node_size_column = "percentage_of_Pathway_detected",
+    save_plot = "png",
+    plot_name = "Enrichment",
+    print_plot = FALSE,
     min_degree = 1,
+    seed = 42,
+    show_density = TRUE,
     max_nodes = 1000
+)
+
+
+viz_graph(
+    as.numeric(r_enrichment$similarity_matrix),
+    r_enrichment$clusters,
+    threshold = 0.5,
+    plot_name = "ClusterGraph",
+    max_nodes = NULL,
+    min_degree = NULL,
+    node_sizes = NULL,
+    show_density = FALSE,
+    seed = NULL,
+    save_plot = "svg",
+    print_plot = TRUE,
+    path = NULL
 ) 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# 
-# 
-# data(intracell_raw)
-# Intra <- intracell_raw[-c(49:58), ] %>% tibble::column_to_rownames("Code")
-# ResI <- dma(
-#     data = Intra[, -c(1:3)],
-#     metadata_sample = Intra[, c(1:3)],
-#     metadata_info = c(
-#         Conditions = "Conditions", Numerator = NULL, Denominator = "HK2"
-#     )
-# )
